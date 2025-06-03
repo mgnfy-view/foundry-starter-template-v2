aff7ede6ad1624710ac7b8a685b7122710cb4a6b
Sahil-Gujrati <sahilgujrati12@gmail.com>
2025-06-03 15:00:30 +0530

    feat: Add formatting options, and format contracts

--- script/DeployHelloWorld.s.sol
+++ script/DeployHelloWorld.s.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {Script} from "forge-std-1.9.7/src/Script.sol";
+import { Script } from "forge-std-1.9.7/src/Script.sol";
 
-import {HelloWorld} from "@src/HelloWorld.sol";
+import { HelloWorld } from "@src/HelloWorld.sol";
 
 contract DeployHelloWorld is Script {
     HelloWorld public helloWorld;
--- test/HelloWorld.t.sol
+++ test/HelloWorld.t.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {Test, console} from "forge-std-1.9.7/src/Test.sol";
+import { Test, console } from "forge-std-1.9.7/src/Test.sol";
 
-import {HelloWorld} from "@src/HelloWorld.sol";
+import { HelloWorld } from "@src/HelloWorld.sol";
 
 contract HelloWorldTests is Test {
     HelloWorld public helloWorld;
